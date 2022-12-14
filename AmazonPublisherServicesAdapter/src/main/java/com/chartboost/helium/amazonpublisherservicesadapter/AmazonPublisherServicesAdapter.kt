7deef1a0a25df14840710e18c81ad29642d48a35
Vu <vu.chau@chartboost.com>
2022-12-14 17:54:02 -0500

    Adapter Audits, Dec 14 (#20)

--- AmazonPublisherServicesAdapter/src/main/java/com/chartboost/helium/amazonpublisherservicesadapter/AmazonPublisherServicesAdapter.kt
+++ AmazonPublisherServicesAdapter/src/main/java/com/chartboost/helium/amazonpublisherservicesadapter/AmazonPublisherServicesAdapter.kt
@@ -84,9 +84,14 @@ class AmazonPublisherServicesAdapter : PartnerAdapter {
     /**
      * Get the APS adapter version.
      *
-     * Note that the version string will be in the format of `Helium.Partner.Partner.Partner.Adapter`,
-     * in which `Helium` is the version of the Helium SDK, `Partner` is the major.minor.patch version
-     * of the partner SDK, and `Adapter` is the version of the adapter.
+     * You may version the adapter using any preferred convention, but it is recommended to apply the
+     * following format if the adapter will be published by Helium:
+     *
+     * Helium.Partner.Adapter
+     *
+     * "Helium" represents the Helium SDK’s major version that is compatible with this adapter. This must be 1 digit.
+     * "Partner" represents the partner SDK’s major.minor.patch.x (where x is optional) version that is compatible with this adapter. This can be 3-4 digits.
+     * "Adapter" represents this adapter’s version (starting with 0), which resets to 0 when the partner SDK’s version changes. This must be 1 digit.
      */
     override val adapterVersion: String
         get() = BuildConfig.HELIUM_APS_ADAPTER_VERSION
@@ -539,6 +544,7 @@ class AmazonPublisherServicesAdapter : PartnerAdapter {
     /**
      * Attempt to load an APS interstitial ad.
      *
+     * @param context The current [Context].
      * @param request An [PartnerAdLoadRequest] instance containing data to load the ad with.
      * @param partnerAdListener A [PartnerAdListener] to notify Helium of ad events.
      *
