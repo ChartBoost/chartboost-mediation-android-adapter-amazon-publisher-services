fe0fb6020ddb6299c40cbf5e67fb50e0c16b09f4
Bichen Wang <bichenwang@gmail.com>
2023-03-24 12:30:01 -0700

    APS 4.9.6.2.1

--- AmazonPublisherServicesAdapter/build.gradle.kts
+++ AmazonPublisherServicesAdapter/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         minSdk = 21
         targetSdk = 33
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.9.7.0.1"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "4.9.6.2.1"
         buildConfigField("String", "CHARTBOOST_MEDIATION_APS_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -70,7 +70,7 @@ dependencies {
     "remoteImplementation"("com.chartboost:chartboost-mediation-sdk:4.0.0")
 
     //Partner SDK
-    implementation("com.amazon.android:aps-sdk:9.7.0")
+    implementation("com.amazon.android:aps-sdk:9.6.2")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
