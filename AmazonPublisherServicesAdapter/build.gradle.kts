ad905cbeb3afb60f025ceef74e153cc7bd9a6381
Bichen Wang <bichenwang@gmail.com>
2024-08-01 13:33:54 -0700

    java 17

--- AmazonPublisherServicesAdapter/build.gradle.kts
+++ AmazonPublisherServicesAdapter/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         minSdk = 21
         targetSdk = 34
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.9.10.1.0"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.9.10.2.0"
         buildConfigField("String", "CHARTBOOST_MEDIATION_APS_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
 
         consumerProguardFiles("proguard-rules.pro")
@@ -91,17 +91,8 @@ dependencies {
     "candidateImplementation"("com.chartboost:chartboost-mediation-sdk:5.0.0")
 
     // Partner SDK
-<<<<<<< HEAD
-<<<<<<< HEAD
     implementation("com.amazon.android:aps-sdk:9.10.2")
     implementation("com.iabtcf:iabtcf-decoder:2.0.10")
-=======
-    implementation("com.amazon.android:aps-sdk:9.9.5")
->>>>>>> 2a20fa0 ([HB-7689] Version pumps for Mediation 5 (#70))
-=======
-    implementation("com.amazon.android:aps-sdk:9.10.1")
-    implementation("com.iabtcf:iabtcf-decoder:2.0.10")
->>>>>>> 608b8ef ([HB-7689] Catching up with latest certified versions (#78))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
 
