0ca275cb4833101fb9c28f318d233b3f7ab16267
Ray Graham <rayg@bitwisedesign.com>
2025-08-06 12:18:46 -0700

    5.10.1.1.0

--- AmazonPublisherServicesAdapter/build.gradle.kts
+++ AmazonPublisherServicesAdapter/build.gradle.kts
@@ -42,7 +42,7 @@ android {
         minSdk = 21
         targetSdk = 34
         // If you touch the following line, don't forget to update scripts/get_rc_version.zsh
-        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.9.10.3.0"
+        android.defaultConfig.versionName = System.getenv("VERSION_OVERRIDE") ?: "5.10.1.1.0"
         buildConfigField("String", "CHARTBOOST_MEDIATION_APS_ADAPTER_VERSION", "\"${android.defaultConfig.versionName}\"")
 
         consumerProguardFiles("proguard-rules.pro")
@@ -91,7 +91,7 @@ dependencies {
     "candidateImplementation"("com.chartboost:chartboost-mediation-sdk:5.0.0")
 
     // Partner SDK
-    implementation("com.amazon.android:aps-sdk:9.10.3")
+    implementation("com.amazon.android:aps-sdk:10.1.1")
     implementation("com.iabtcf:iabtcf-decoder:2.0.10")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.4.1")
